3e4a806d1773e917470946359fdd2644bb559073
anvithapo99 <53563539+anvithapo99@users.noreply.github.com>
2019-09-10 08:09:59 +0530

    Update Hamiltonian.cpp

--- Hamiltonian.cpp
+++ Hamiltonian.cpp
@@ -1,6 +1,6 @@
-Hamiltonian and Lagrangian
-Students have become secret admirers of SEGP. They find the course exciting and the professors amusing. After a superb Mid Semester examination, it’s now time for the results. The TAs have released the marks of students in the form of an array, where arr[i] represents the marks of the ith student.
-Since you are a curious kid, you want to find all the marks that are not smaller than those on its right side in the array.
+/*Hamiltonian and Lagrangian
+Students have become secret admirers of SEGP. They find the course exciting and the professors amusing. After a superb Mid Semester examination, itâ€™s now time for the results. The TAs have released the marks of students in the form of an array, where arr[i] represents the marks of the ith student.
+Since you are a curious kid, you want to find all the marks that are not smaller than those on its right side in the array.*/
 
 #include<iostream>
  
